0ed5d8554c16c1fd9108cfa4009802d72656f6d3
Mr-Wei <965915958@qq.com>
2018-01-23 22:23:39 +0800

    测试

--- src/main/java/com/wdd/skill/service/RedisService.java
+++ src/main/java/com/wdd/skill/service/RedisService.java
@@ -1,5 +1,6 @@
 package com.wdd.skill.service;
 
+import com.wdd.skill.redis.KeyPrefix;
 import com.wdd.skill.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,38 +19,54 @@ public class RedisService {
     private JedisPool jedisPool;
 
 
-    public <T> T get(String key, Class<T> clazz) {
+    /**
+     * 获取当个对象
+     * */
+    public <T> T get(KeyPrefix prefix, String key, Class<T> clazz) {
         Jedis jedis = null;
         try {
-            jedis = jedisPool.getResource();
-            String str = jedis.get(key);
-            T t = JsonUtil.string2Bean(str, clazz);
+            jedis =  jedisPool.getResource();
+            //生成真正的key
+            String realKey  = prefix.getPrefix() + key;
+            String  str = jedis.get(realKey);
+            T t =  JsonUtil.string2Bean(str, clazz);
             return t;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (jedis != null) {
+        }finally {
+            if(jedis != null) {
                 jedis.close();
             }
         }
-        return null;
     }
 
-    public <T> boolean set(String key, T value) {
+    /**
+     * 设置对象
+     * */
+    public <T> boolean set(KeyPrefix prefix, String key,  T value) {
         Jedis jedis = null;
         try {
-            jedis = jedisPool.getResource();
-            jedis.set(key, JsonUtil.bean2String(value));
+            jedis =  jedisPool.getResource();
+            String str = JsonUtil.bean2String(value);
+            if(str == null || str.length() <= 0) {
+                return false;
+            }
+            //生成真正的key
+            String realKey  = prefix.getPrefix() + key;
+            int seconds =  prefix.expireSeconds();
+            if(seconds <= 0) {
+                jedis.set(realKey, str);
+            }else {
+                jedis.setex(realKey, seconds, str);
+            }
             return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (jedis != null) {
+        }finally {
+            if(jedis != null) {
                 jedis.close();
             }
         }
-        return false;
     }
+
+
+
 
 
 }
