c2972817e7587b11460dec73052662fd30c8ecac
Mr-Wei <965915958@qq.com>
2018-02-18 18:56:07 +0800

    测试

--- src/main/java/com/wdd/skill/util/MD5Util.java
+++ src/main/java/com/wdd/skill/util/MD5Util.java
@@ -10,6 +10,8 @@ import org.apache.commons.codec.digest.DigestUtils;
  **/
 public class MD5Util {
 
+    private static final String salt="1a12234";
+
     /**
      * 将字符串转化为md5
      * @param str
@@ -18,6 +20,16 @@ public class MD5Util {
     public static String md5(String str) {
         return DigestUtils.md5Hex(str);
 
+    }
+
+    /**
+     * 将输入的密码转化为加密的密码
+     * @param pass
+     * @return
+     */
+    public static String inputPass2formPass(String pass){
+        String str=salt.charAt(0)+salt.charAt(1)+pass+salt.charAt(5)+salt.charAt(4);
+        return md5(str);
     }
 
 }
